a8d0107e88d8298c49ee332f4be7cda2cb2b12e6
marino5710 <brianmarin4@gmail.com>
2023-05-18 11:45:46 -0600

    se crearon las variables para almacenar los campos de la bd

--- modelos/Producto.php
+++ modelos/Producto.php
@@ -2,5 +2,9 @@
 require 'Conexion.php'; 
 
 class Producto extends Conexion{
-    
+    public $producto_id; 
+    public $producto_nombre; 
+    public $producto_precio; 
+    public $producto_situacion; 
+
 }
