c943700f8f07df55023b3f039ad594e1f3da3e96
marino5710 <brianmarin4@gmail.com>
2023-05-18 11:48:22 -0600

    se creo el metodo constructor

--- modelos/Producto.php
+++ modelos/Producto.php
@@ -7,4 +7,12 @@ class Producto extends Conexion{
     public $producto_precio; 
     public $producto_situacion; 
 
+    public function __construct($args = [ ])
+    {
+        $this->producto_id = $args['producto_id'];
+        $this->producto_nombre = $args['producto_nombre'];
+        $this->producto_precio = $args['producto_precio'];
+        $this->producto_situacion = $args['producto_situacion'];
+    }
+
 }
