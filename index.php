0ce8bece45bdcf6701fbd7c1d4d58c86bb7cd44a
marino5710 <brianmarin4@gmail.com>
2023-05-23 14:11:53 -0600

    validacion de datos fromulario

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    // require  './modelos/Conexion.php'; 
-    // $resultado = Conexion::ejecutar("INSERT INTO productos(producto_nombre, producto_precio)values('prueba', '5')"); 
-    // var_dump($resultado);
-
-    require  './modelos/Conexion.php'; 
-    $resultado = Conexion::servir("SELECT * FROM productos"); 
-    var_dump($resultado);
-    echo "</pre>"; 
